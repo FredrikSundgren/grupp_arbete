5f3bace27e303e43a5cf125211a27f4224292f85
Tova0811 <91182586+Tova0811@users.noreply.github.com>
2021-11-26 17:32:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,55 +3,56 @@ let titleArray = [];
 let descArray = [];
 let lastshownprod;
 let previousActiveElement;
-const KEYCODE = {ESC: 27};
-const exbtn = document.querySelector('.exbtn');
-const dialog = document.querySelector('.dialog');
-const dialogimg = dialog.querySelector('.dialog__img');
-const dialogtext = dialog.querySelector('.dialog__info');
-const dialogmask = dialog.querySelector('.dialog__mask');
-const shopwindow = dialog.querySelector('#shop-window');
-
+const KEYCODE = {
+    ESC: 27
+};
+const exbtn = document.querySelector(".exbtn");
+const dialog = document.querySelector(".dialog");
+const dialogimg = dialog.querySelector(".dialog__img");
+const dialogtext = dialog.querySelector(".dialog__info");
+const dialogmask = dialog.querySelector(".dialog__mask");
+const shopwindow = dialog.querySelector("#shop-window");
+const main = document.querySelector(".flex__products");
+const sort = document.querySelector("#sort__option")
 fetch("./images.json")
     .then((response) => response.json())
 
-.then((data) => {
-    console.log(data);
+    .then((data) => {
+        console.log(data);
 
-    let imgData = data;
+        let imgData = data;
 
-    imgData.forEach(images =>
-        {
+        imgData.forEach((images) => {
             let div = document.createElement("div");
             div.setAttribute("class", "shop-div");
-            document.body.appendChild(div);
+            main.append(div);
 
             imgArray.push(images.url);
             titleArray.push(images.title);
             descArray.push(images.info);
 
-            div.innerHTML += `<h2>${images.title}</h2> <img class=${images.class} src=${images.url}>`;
+
+            div.innerHTML += ` <div class='img__block'> <img class=${images.class} src=${images.url}>
+        <h2>${images.title}</h2> <h3>Storlek: ${images.info.size}x${images.info.size1}cm     Pris: ${images.info.price}kr</h3></div>`;
         });
 
-    let showimg = document.querySelectorAll(".popup-img");
+        let showimg = document.querySelectorAll(".popup-img");
 
-    if(showimg)
-    {
-        showimg.forEach(function(img, index)
-            {
-                img.onclick = function()
-                {
+        if (showimg) {
+            showimg.forEach(function (img, index) {
+                img.onclick = function () {
                     lastshownprod = index;
                     let i = lastshownprod;
                     popUpWindow();
-        
+
                     let title = document.createElement("h2");
                     title.setAttribute("class", "shop-popup-title");
                     dialogtext.appendChild(title);
-        
+
                     let img = document.createElement("img");
-                    img.setAttribute("class", "shop-popup-img")
+                    img.setAttribute("class", "shop-popup-img");
                     dialogimg.appendChild(img);
-        
+
                     let desc = document.createElement("p");
                     desc.setAttribute("class", "shop-popup-desc");
                     dialogtext.appendChild(desc);
@@ -59,6 +60,10 @@ fetch("./images.json")
                     let size = document.createElement("p");
                     size.setAttribute("class", "shop-popup-size");
                     dialogtext.appendChild(size);
+
+                    let size1 = document.createElement("p");
+                    size1.setAttribute("class", "shop-popup-size");
+                    dialogtext.appendChild(size1);
 
                     let price = document.createElement("p");
                     price.setAttribute("class", "shop-popup-price");
@@ -69,46 +74,44 @@ fetch("./images.json")
                     btn.setAttribute("id", "shop-popup-btn");
                     dialogtext.appendChild(btn);
                     btn.innerHTML = "Köp";
-        
+
                     img.setAttribute("src", imgArray[i]);
                     title.innerHTML = titleArray[i];
                     desc.innerHTML = descArray[i].description;
-                    size.innerHTML = "Storlek: " + descArray[i].size;
-                    price.innerHTML = "Pris: " + descArray[i].price;
+                    size.innerHTML = "Storlek: " + descArray[i].size + "x" + descArray[i].size1 + "cm";
+                    price.innerHTML = "Pris: " + descArray[i].price + "cm";
 
-                    function popUpWindow()
-                    {
+                    function popUpWindow() {
                         previousActiveElement = document.activeElement;
-                        
-                        dialog.classList.add('opened');
 
-                        dialogmask.addEventListener('click', closeDialog);
-                        exbtn.addEventListener('click', closeDialog);
-                        document.addEventListener('keydown', checkCloseDialog);
+                        dialog.classList.add("opened");
 
-                        dialog.querySelector('.exbtn').focus();
+                        dialogmask.addEventListener("click", closeDialog);
+                        exbtn.addEventListener("click", closeDialog);
+                        document.addEventListener("keydown", checkCloseDialog);
+
+                        dialog.querySelector(".exbtn").focus();
                     }
 
-                    function checkCloseDialog(press)
-                    {
-                        if(press.keyCode === KEYCODE.ESC)
-                        closeDialog();
+                    function checkCloseDialog(press) {
+                        if (press.keyCode === KEYCODE.ESC) closeDialog();
                     }
 
-                    function closeDialog()
-                    {
-                        dialogmask.removeEventListener('click', closeDialog);
-                        exbtn.removeEventListener('click', closeDialog);
-                        document.removeEventListener('keydown', checkCloseDialog);
+                    function closeDialog() {
+                        dialogmask.removeEventListener("click", closeDialog);
+                        exbtn.removeEventListener("click", closeDialog);
+                        document.removeEventListener("keydown", checkCloseDialog);
                         document.querySelector("#shop-popup-btn").remove();
 
                         dialogtext.innerHTML = "";
                         dialogimg.innerHTML = "";
 
-                        dialog.classList.remove('opened');
+                        dialog.classList.remove("opened");
                         previousActiveElement.focus();
                     }
-                }
+                };
             });
-    }
-});
+        }
+
+
+    });
